d7b18c353b013cc363e65257b326116b7614d1fb
ahmad malek <ahmadmalek3@outlook.com>
2023-06-18 16:55:12 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-/* this is a print program for specific sentence */
+/**
+ *  this is a print program for specific sentence
+ *
+ *  the main is the entry point of the program
+ *
+ *  return is zero her
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
