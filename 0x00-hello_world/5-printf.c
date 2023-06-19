e052a9e5e0d6baa2ae17748986430c9185b7cc98
ahmad malek <ahmadmalek3@outlook.com>
2023-06-18 17:10:28 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: This C program is about print by printf
+ *
+ * Return:Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\,");
+	return (0);
+}
