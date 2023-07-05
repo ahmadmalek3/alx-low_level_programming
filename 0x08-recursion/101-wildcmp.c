57077dff8a2c4edf32cba8fad2b363213a6a38ef
ahmadmalek3 <ahmadmalek3@outlook.com>
2023-07-05 23:29:57 +0000

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,19 +11,21 @@
 int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '\0')
-	return (*s1 == '\0');
+		return (*s1 == '\0');
 
 	if (*s2 == '*')
 	{
-	if (*(s2 + 1) == '*')
-	return (wildcmp(s1, s2 + 1));
-	if (wildcmp(s1, s2 + 1))
-	return (1);
-	return (wildcmp(s1 + 1, s2));
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (*(s2 + 1) == '\0')
+			return (1);
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+			return (1);
+		return (wildcmp(s1, s2 + 1));
 	}
 
 	if (*s1 != *s2)
-	return (0);
+		return (0);
 
 	return (wildcmp(s1 + 1, s2 + 1));
 }
