ad395cc6b57eb33d320f93347b55a95daff6840b
ahmadmalek3 <ahmadmalek3@outlook.com>
2023-09-02 05:13:05 +0300

    I get a lot of things from this task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - the entry of the program
+ *
+ * Description: this program prints all possible different
+ * combinations of two digits.
+ *
+ * Return: Always (0)succeed
+**/
+
+int main(void)
+{
+	/*initialize the two variables for two digits*/
+	int firstdigit, seconddigit;
+
+	/*for loop for the first digit*/
+	for (firstdigit = 0; firstdigit <= 8; firstdigit++)
+	{
+		for (seconddigit = firstdigit + 1; seconddigit <= 9; seconddigit++)
+		{
+			putchar(firstdigit + '0');
+			putchar(seconddigit + '0');
+			
+			if (firstdigit != 8 || seconddigit != 9)
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
