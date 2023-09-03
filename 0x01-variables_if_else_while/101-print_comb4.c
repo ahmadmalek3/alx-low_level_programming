f288c289f83a6131b112e76a73edce1f5b1fba06
ahmadmalek3 <ahmadmalek3@outlook.com>
2023-09-03 06:58:07 +0300

    My second chance after the last problem

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - The entry of the program
+ *
+ * Description: This program is for printing all possible
+ * combination of three digits
+ *
+ * Return: Always (0) succeed
+**/
+
+int main(void)
+{
+	int first, second, third;
+
+	for (first = 0; first <= 8; first++)
+	{
+		for (second = first + 1; second <= 8; second++)
+		{
+			for (third = second + 1; third <= 9; third++)
+			{
+				putchar(first + '0');
+				putchar(second + '0');
+				putchar(third + '0');
+
+				if (first != 8 || second != 8 || third != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
