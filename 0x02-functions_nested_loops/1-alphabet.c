cb309634cb3b9dc9c7ff21f990bb8dff20c7818a
ahmadmalek3 <ahmadmalek3@outlook.com>
2023-09-17 23:17:59 +0300

    0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character to the standard output (stdout)
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
+
+/**
+ * print_alphabet - prints the lowercase alphabet followed by a new line
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+	_putchar(letter);
+	letter++;
+	}
+
+	_putchar('\n');
+}
