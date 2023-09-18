8bd2f78b6c18f8f6502aeaa1f4fbd045721fad47
ahmadmalek3 <ahmadmalek3@outlook.com>
2023-09-18 22:56:07 +0300

    shit but i will do it

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,10 @@
 
 void print_alphabet_x10(void)
 {
-	char alpha1, alpha2;
+	int alpha;
+	char alpha;
 
-	for (alpha1 = 'a'; alpha1 <= 'z'; alpha1++)
+	for (alpha =1 ; alpha <= 10; alpha++)
 	{
 		for (alpha2 = 'a'; alpha2 <= 'z'; alpha2++)
 		{
