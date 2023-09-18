d7604c36299e446f2d6a144e22e85dfb1b66a591
ahmadmalek3 <ahmadmalek3@outlook.com>
2023-09-18 22:59:46 +0300

    i am sure i did it

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,10 @@
 
 void print_alphabet_x10(void)
 {
-	int alpha;
-	char alpha;
+	int alpha1;
+	char alpha2;
 
-	for (alpha =1 ; alpha <= 10; alpha++)
+	for (alpha1 =1 ; alpha1 <= 10; alpha1++)
 	{
 		for (alpha2 = 'a'; alpha2 <= 'z'; alpha2++)
 		{
