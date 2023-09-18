c0d4ffe6b5e795d298acd178fb3bd39eef83d7fb
ahmadmalek3 <ahmadmalek3@outlook.com>
2023-09-18 23:01:09 +0300

    this time i am sure

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	int alpha1;
 	char alpha2;
 
-	for (alpha1 =1 ; alpha1 <= 10; alpha1++)
+	for (alpha1 = 1 ; alpha1 <= 10; alpha1++)
 	{
 		for (alpha2 = 'a'; alpha2 <= 'z'; alpha2++)
 		{
