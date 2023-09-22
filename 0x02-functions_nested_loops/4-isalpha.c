e9b9a0c79edbdff9462cdf5fa4a9b26c6d5bd5ae
ahmadmalek3 <ahmadmalek3@outlook.com>
2023-09-23 00:37:51 +0300

    problem number 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - is a function check if the input alphabet or not
+ *
+ * @c: is the input
+ *
+ * Return: 1 if the c is alpha and else return 0
+**/
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
