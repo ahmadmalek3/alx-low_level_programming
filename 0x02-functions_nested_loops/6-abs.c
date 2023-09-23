8502f8f01cec5ba8faca44f2e0d08591bc12466b
ahmadmalek3 <ahmadmalek3@outlook.com>
2023-09-23 11:19:26 +0300

    66

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,13 @@
 
 /**
  * _abs - finction that prints the absolute value of an integer
- * @int: the inpt we make positive always
+ * @x: the inpt we make positive always
  * Return: if positive return the real number and if
  * negative return the neg of the value
 **/
 
-int _abs(int)
+int _abs(int x)
 {
-	int x;
 
 	if (x >= 0)
 	{
