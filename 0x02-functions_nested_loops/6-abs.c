9f4331db6fd3248bc0d0db638ca208a4c0f90e21
ahmadmalek3 <ahmadmalek3@outlook.com>
2023-09-23 11:17:53 +0300

    i think it would be correct

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - finction that prints the absolute value of an integer
+ * @int: the inpt we make positive always
+ * Return: if positive return the real number and if
+ * negative return the neg of the value
+**/
+
+int _abs(int)
+{
+	int x;
+
+	if (x >= 0)
+	{
+		return (x);
+	}
+	if (x < 0)
+	{
+		return (-x);
+	}
+}
