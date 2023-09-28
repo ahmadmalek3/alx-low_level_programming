c23d02d544a2d15927f47f3154599be9765cb256
ahmadmalek3 <ahmadmalek3@outlook.com>
2023-09-28 18:39:13 +0300

    6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Check if a number is prime recursively.
+ * @n: The integer to check for primality.
+ *
+ * Return: 1 if 'n' is prime, 0 otherwise.
+ */
+int is_prime_number(int n)
+{
+	return (is_prime_recursive(n, 2)); /* Start checking divisors from 2. */
+}
+
+/**
+ * is_prime_recursive - Check if a number is prime recursively.
+ * @n: The integer to check for primality.
+ * @divisor: The current divisor to check.
+ *
+ * Return: 1 if 'n' is prime, 0 otherwise.
+ */
+int is_prime_recursive(int n, int divisor)
+{
+	if (n < 2) /* Numbers less than 2 are not prime. */
+	{
+	return (0);
+	}
+	if (n == 2) /* 2 is prime. */
+	{
+	return (1);
+	}
+	if (divisor * divisor > n)
+	{
+	return (1);
+	}
+	if (n % divisor == 0) /* 'n' is divisible by 'divisor', so it's not prime. */
+	{
+	return (0);
+	}
+	return (is_prime_recursive(n, divisor + 1)); /* Try the next divisor. */
+}
