6a89cd36f2ca600bba9730fabd81afd7cf7eeec8
ahmadmalek3 <ahmadmalek3@outlook.com>
2023-10-11 01:09:09 +0300

    4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - Creates a new dog and stores a copy of name and owner.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: A pointer to the newly created dog, or NULL if the function fails.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog_ptr;
+	char *name_copy, *owner_copy;
+
+	new_dog_ptr = malloc(sizeof(dog_t));
+	if (new_dog_ptr == NULL)
+	return (NULL);
+
+	name_copy = strdup(name);
+	owner_copy = strdup(owner);
+
+	if (name_copy == NULL || owner_copy == NULL)
+	{
+	free(new_dog_ptr);
+	free(name_copy);
+	free(owner_copy);
+	return (NULL);
+	}
+
+	new_dog_ptr->name = name_copy;
+	new_dog_ptr->age = age;
+	new_dog_ptr->owner = owner_copy;
+
+	return (new_dog_ptr);
+}
