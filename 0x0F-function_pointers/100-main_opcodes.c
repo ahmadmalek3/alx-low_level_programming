c819afb59bf322deea432b0037fa0e4cebf0a411
ahmadmalek3 <ahmadmalek3@outlook.com>
2023-10-11 23:41:46 +0300

    100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * print_opcodes - Print the opcodes of a function.
+ * @func: A pointer to the function.
+ * @num_bytes: The number of bytes to print.
+ */
+void print_opcodes(void (*func)(), int num_bytes)
+{
+	unsigned char *opcodes = (unsigned char *)func;
+
+	for (int i = 0; i < num_bytes; i++)
+	{
+	printf("%02x", opcodes[i]);
+	if (i < num_bytes - 1)
+		printf(" ");
+	}
+	printf("\n");
+}
+
+/**
+ * main - Entry point.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ *
+ * Return: 0 for success, 1 for incorrect argument count, 2 for negative bytes.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+	printf("Error\n");
+	return (1);
+	}
+
+	int num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0) {
+	printf("Error\n");
+	return (2);
+	}
+
+	print_opcodes((void *)&main, num_bytes);
+
+	return (0);
+}
